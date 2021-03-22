cdcd13c6bed3580397cd46097c0f8584fa03f8fd
mkshnik <63475386+mkshnik@users.noreply.github.com>
2021-03-22 19:58:24 +0900

    Add files via upload

---
+++ PepperSpolecnik.pml
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="12cv" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="feedparser" src="feedparser.py" />
+        <File name="dance_song" src="dance_song.mp3" />
+        <File name="swiftswords_ext" src="behavior_1/swiftswords_ext.mp3" />
+        <File name="taichimove" src="behavior_1/taichimove.pmt" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ translation_en_US.ts
@@ -0,0 +1,373 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Autorize/Error age recognition</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Chyba v rozpoznání věku, podívejte se prosím na mě ještě jednou!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Chyba v rozpoznání věku, podívejte se prosím na mě ještě jednou!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Autorize/Error face recognition</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Chyba v rozpoznání obličeje, podívejte se prosím na mě ještě jednou!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Chyba v rozpoznání obličeje, podívejte se prosím na mě ještě jednou!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Autorize/Get Localized Text</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>user</source>
+            <comment>Text</comment>
+            <translation type="unfinished">user</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Autorize/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Jak se jmenuješ?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Jak se jmenuješ?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Autorize/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Podívej se na mě, zapamatuju tvůj obličej a věk!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Podívej se na mě, zapamatuju tvůj obličej a věk!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Basic configurations/Say</name>
+        <message>
+            <source>Ahoj, ja jsem Pepper! Zeptej se mě Co umíš? jestli chceš dozvědět o mých možnostech</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ahoj, ja jsem Pepper! Zeptej se mě Co umíš? jestli chceš dozvědět o mých možnostech</translation>
+        </message>
+        <message>
+            <source>Ahoj, ja jsem Laura! Zeptej se mě Co umíš? jestli chceš dozvědět o mých možnostech</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ahoj, ja jsem Laura! Zeptej se mě Co umíš? jestli chceš dozvědět o mých možnostech</translation>
+        </message>
+        <message>
+            <source>Ahoj, ja jsem Laura! Zeptej se mě Co umíš? jestli chceš se dozvědět o mých možnostech</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ahoj, ja jsem Laura! Zeptej se mě Co umíš? jestli chceš se dozvědět o mých možnostech</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ahoj, ja jsem ! Zeptej se mě Co umíš? jestli chceš se dozvědět o mých možnostech</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ahoj, ja jsem ! Zeptej se mě Co umíš? jestli chceš se dozvědět o mých možnostech</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Dance/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Chceš zkusit z hudbou?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Chceš zkusit z hudbou?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Dance/Say (1)</name>
+        <message>
+            <source>Jestli chceš by profesionálem, dělej, co já.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Jestli chceš by profesionálem, dělej, co já.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Jestli chceš by profesionálem, dělej, jak já.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Jestli chceš by profesionálem, dělej, jak já.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Dance/Say (2)</name>
+        <message>
+            <source>Vypadá to skvěle. A teď s hudbou.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Vypadá to skvěle. A teď s hudbou.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Dobře. Tak zkusíme to spolu.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Dobře. Tak zkusíme to spolu.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Dance/Say (3)</name>
+        <message>
+            <source>Už skoro jako profesionál.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Už skoro jako profesionál.</translation>
+        </message>
+        <message>
+            <source>Dobře. Tak zkusíme to spolu.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dobře. Tak zkusíme to spolu.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Dance/Say (4)</name>
+        <message>
+            <source>Tak pokračujeme dál.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Tak pokračujeme dál.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Dance/Say (5)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Myslíš že zvládneš to z hudbou?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Myslíš že zvládneš to z hudbou?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Super. Teď jsi jako profesionál.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Super. Teď jsi jako profesionál.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Dance/Say (6)</name>
+        <message>
+            <source>Chceš pokračovat?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Chceš pokračovat?</translation>
+        </message>
+        <message>
+            <source>Tak nic. Pokud budeš chtít zkusit znovu tak si opět řekni tančit.Tak nic. Pokud budeš chtít zkusit znovu tak si opět řekni tančit.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Tak nic. Pokud budeš chtít zkusit znovu tak si opět řekni tančit.Tak nic. Pokud budeš chtít zkusit znovu tak si opět řekni tančit.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Tak nic. Pokud budeš chtít zkusit znovu tak si opět řekni tančit.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Tak nic. Pokud budeš chtít zkusit znovu tak si opět řekni tančit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/DontListen (1)/Say (1)</name>
+        <message>
+            <source>Dotkni se moje hlavy abychse vzbudil!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dotkni se moje hlavy abychse vzbudil!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/DontListen/Say (1)</name>
+        <message>
+            <source>Dotkni se moje hlavy abych zase mohl slyšet!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dotkni se moje hlavy abych zase mohl slyšet!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Game/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ukončuji hru</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ukončuji hru</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Game/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>flepi bird nebo pexeso?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">flepi bird nebo pexeso?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Guess emotion/Say (12)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Nech mě uhádnout jakou máš náladu</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Nech mě uhádnout jakou máš náladu</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Guess emotion/Say (13)</name>
+        <message>
+            <source>Tvoje nálada byla rozpoznána jako</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Tvoje nálada byla rozpoznána jako</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Music/Say (17)</name>
+        <message>
+            <source>Můžeš vybrat mezi žánry: rock, klasika, pop, rep, k-pop, jazz, disko, česká, metal nebo emo</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Můžeš vybrat mezi žánry: rock, klasika, pop, rep, k-pop, jazz, disko, česká, metal nebo emo</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Music/Say (7)</name>
+        <message>
+            <source>Jakou hudbu chceš poslouchat?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Jakou hudbu chceš poslouchat?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Radio/Say (7)</name>
+        <message>
+            <source>Jakou hudbu chceš poslouchat?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Jakou hudbu chceš poslouchat?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Jakou hudbu chceš poslouchat? </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Jakou hudbu chceš poslouchat? </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Na schledanou!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Na schledanou!</translation>
+        </message>
+        <message>
+            <source>Řekni klíčové slovo abych se vypnula!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Řekni klíčové slovo abych se vypnula!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Špatné klíčové slovo</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Špatné klíčové slovo</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Show news with options/Say</name>
+        <message>
+            <source>jaké zprávy si přeješ? sport, kultura, ekonomika nebo obecné zprávy</source>
+            <comment>Text</comment>
+            <translation type="obsolete">jaké zprávy si přeješ? sport, kultura, ekonomika nebo obecné zprávy</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>jaké zprávy si přeješ? sport, kultura, ekonomika nebo obecné zprávy?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">jaké zprávy si přeješ? sport, kultura, ekonomika nebo obecné zprávy?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Show news with options/Say (8)</name>
+        <message>
+            <source>Už jsi dočetl?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Už jsi dočetl?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Show weather forecast/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Řekni mi konec když ukončíš se dívat na počasí.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Řekni mi konec když ukončíš se dívat na počasí.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TakeRest/Say (1)</name>
+        <message>
+            <source>Dotkni se moje hlavy abychse vzbudil!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dotkni se moje hlavy abychse vzbudil!</translation>
+        </message>
+        <message>
+            <source>Dotkni se moje hlavy abychse vzbudila!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dotkni se moje hlavy abychse vzbudila!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Dotkni se moje hlavy abych se vzbudila!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Dotkni se moje hlavy abych se vzbudila!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TurnOff/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Řekni klíčové slovo abych se vypnula!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Řekni klíčové slovo abych se vypnula!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/TurnOff/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Špatné klíčové slovo</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Špatné klíčové slovo</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/dnc/Say (1)</name>
+        <message>
+            <source>Jestli chceš by profesionálem, dělej, co já.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Jestli chceš by profesionálem, dělej, co já.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/dnc/Say (2)</name>
+        <message>
+            <source>Vypadá to skvěle. A teď s hudbou.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Vypadá to skvěle. A teď s hudbou.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/dnc/Say (3)</name>
+        <message>
+            <source>Už skoro jako profesionál.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Už skoro jako profesionál.</translation>
+        </message>
+    </context>
+</TS>
